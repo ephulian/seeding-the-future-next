9beaa975d7ebc71dbb0bb72f3728016a3ee1c82b
Kiril Komitski <k.komitski94@gmail.com>
2022-04-18 14:08:25 +0100

    CORS header

--- next.config.js
+++ next.config.js
@@ -1,14 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	async rewrites() {
+
+	async headers() {
 		return [
 			{
-				source: '/api/openai/manifesto',
-				destination: 'https://seeding-the-future-next.vercel.app/api/openai/manifesto',
+				// matching all API routes
+				source: '/api/:path*',
+				headers: [
+					{ key: 'Access-Control-Allow-Credentials', value: 'true' },
+					{ key: 'Access-Control-Allow-Origin', value: '*' },
+					{ key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+					{
+						key: 'Access-Control-Allow-Headers',
+						value:
+							'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+					},
+				],
 			},
 		];
 	},
+	// async rewrites() {
+	// 	return [
+	// 		{
+	// 			source: '/api/openai/manifesto',
+	// 			destination: 'https://seeding-the-future-next.vercel.app/api/openai/manifesto',
+	// 		},
+	// 	];
+	// },
 };
 
 module.exports = nextConfig;
--- pages/api/openai/manifesto.js
+++ pages/api/openai/manifesto.js
@@ -1,23 +1,6 @@
 import { openai } from '../config/openai-config';
 
-const allowCors = (fn) => async (req, res) => {
-	res.setHeader('Access-Control-Allow-Credentials', true);
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	// another common pattern
-	// res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-	res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-	res.setHeader(
-		'Access-Control-Allow-Headers',
-		'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-	);
-	if (req.method === 'OPTIONS') {
-		res.status(200).end();
-		return;
-	}
-	return await fn(req, res);
-};
-
-async function handler(req, res) {
+export default async function handler(req, res) {
 	// if (req.method === 'GET') {
 	// 	res.status(200).json();
 	// } else
@@ -43,5 +26,3 @@ async function handler(req, res) {
 			});
 	}
 }
-
-module.exports = allowCors(handler);
