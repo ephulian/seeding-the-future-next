ed59cb130818a0ad9bdf6367fe1ad9944f933a80
Kiril Komitski <k.komitski94@gmail.com>
2022-04-18 13:18:21 +0100

    Server ref fixed

--- pages/api/config/index.ts
+++ pages/api/config/index.ts
@@ -1,4 +1,4 @@
 // const dev = process.env.NODE_ENV !== 'production';
 
 // export const server = dev ? 'http://localhost:3000' : 'https"//yourwebsite.com';
-export const server = 'http://localhost:3000';
+export const server = 'https://seeding-the-future-next-ephulian.vercel.app';
