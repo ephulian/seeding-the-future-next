5a4ffaeee7d7f8bcb86cfbc3654f1081f3cd407a
Kiril Komitski <k.komitski94@gmail.com>
2022-04-18 14:17:44 +0100

    Final again

--- pages/api/firestore/answers.js
+++ pages/api/firestore/answers.js
@@ -1,7 +1,24 @@
 import { addDoc, collection } from 'firebase/firestore';
 import { db } from '../config/firebase-config';
 
-export default async function handler(req, res) {
+const allowCors = (fn) => async (req, res) => {
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	// another common pattern
+	// res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+	res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+	);
+	if (req.method === 'OPTIONS') {
+		res.status(200).end();
+		return;
+	}
+	return await fn(req, res);
+};
+
+async function handler(req, res) {
 	const answersRef = collection(db, 'answers');
 
 	if (req.method === 'POST') {
@@ -19,3 +36,5 @@ export default async function handler(req, res) {
 		}
 	}
 }
+
+export default allowCors(handler);
