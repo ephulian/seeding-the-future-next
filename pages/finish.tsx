c14d9750304e68248a844c8b4a2f44ad0784fefd
Kiril Komitski <k.komitski94@gmail.com>
2022-04-18 14:16:42 +0100

    Final

--- pages/finish.tsx
+++ pages/finish.tsx
@@ -26,10 +26,10 @@ export default function Finish() {
 		setManifesto('Generating....');
 		axios
 			.post(`${server}/api/openai/manifesto`, {
-				// question: `Q: ${answers[1].Q} \nA: ${answers[1].A}\n\nQ: ${answers[2].Q}\nA: ${answers[2].A}\n\nQ: ${answers[3].Q}\nA: ${answers[3].A}\n\nQ: ${answers[4].Q}\nA: ${answers[4].A}\n\nQ: ${answers[5].Q}\nA: ${answers[5].A}\n\nQ: ${answers[6].Q}\nA: ${answers[6].A}\n\nQ: Generate a manifesto?`,
+				question: `Q: ${answers[1].Q} \nA: ${answers[1].A}\n\nQ: ${answers[2].Q}\nA: ${answers[2].A}\n\nQ: ${answers[3].Q}\nA: ${answers[3].A}\n\nQ: ${answers[4].Q}\nA: ${answers[4].A}\n\nQ: ${answers[5].Q}\nA: ${answers[5].A}\n\nQ: ${answers[6].Q}\nA: ${answers[6].A}\n\nQ: Generate a manifesto?`,
 				// question:
 				// 	'Q: Use a word to describe the feelings of post-pandemic times?\n\nA: Anxious.\n\nQ: What truly matters to you in the future world?\n\nA: Peace.\n\nQ: Use a word to define the future city?\n\nA: Spectacular.\n\nQ: Who is the right partner if you were to start a business in 2030?\n\nA: A. Someone who is competitive, ambitious, and able to deal with uncertainty.\n\nQ: Imagine you are a fashion brand CEO selecting a co-creator, from different industry, to collaborate with on a social media platform. Who is the best person?\n\nA: B. Journalist or blogger elected by the audience by voting\n\nQ: The best society in the future is one where people? A: D. Have equal income and opportunities.\n\nQ: Generate a manifesto?',
-				question: 'this is a test',
+				// question: 'this is a test',
 			})
 			.then((res) => {
 				setAnswer(res.data.choices[0].text.substring(2));
