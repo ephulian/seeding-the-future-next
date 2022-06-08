ae1d8e84e8a83e0e6c5566412fc0b0f5e0de41ca
Kiril Komitski <k.komitski94@gmail.com>
2022-06-08 11:34:58 +0100

    icon again

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,6 +10,11 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
 			{/* <Meta /> */}
 			<Head>
 				<title>Cloudia</title>
+				<link
+					rel='icon'
+					type='image/ico'
+					href='https://firebasestorage.googleapis.com/v0/b/stf-db.appspot.com/o/logo.png?alt=media&token=ab5e73ec-47d8-4102-be7a-06911d50be27'
+				/>
 			</Head>
 			<main>{children}</main>
 		</>
