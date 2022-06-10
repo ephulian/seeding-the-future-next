e6b3d7efd507f773b41d96ed6b2a522bb809fe72
Kiril Komitski <k.komitski94@gmail.com>
2022-06-10 12:12:25 +0100

    fix?

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,8 +3,16 @@
 // import Meta from './Meta';
 
 import Head from 'next/head';
+import { useEffect, useRef } from 'react';
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
+	const main: any = useRef();
+
+	useEffect(() => {
+		// main.current.style.border = '1px solid red';
+		main.current.style.height = window.innerHeight;
+	});
+
 	return (
 		<>
 			{/* <Meta /> */}
@@ -16,7 +24,8 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
 					href='https://firebasestorage.googleapis.com/v0/b/stf-db.appspot.com/o/logo.png?alt=media&token=ab5e73ec-47d8-4102-be7a-06911d50be27'
 				/>
 			</Head>
-			<main>{children}</main>
+			{/* <main>{children}</main> */}
+			<main ref={main}>{children}</main>
 		</>
 	);
 };
