320020182dc9a8181fa28c3424c1be230cb590fc
Kiril Komitski <k.komitski94@gmail.com>
2022-06-10 12:56:57 +0100

    again

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,18 @@ import type { AppProps } from 'next/app';
 import { Layout } from '../components/Layout';
 import { store } from '../store';
 import { Provider } from 'react-redux';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+	const resetHeight = () => {
+		document.body.style.height = window.innerHeight + 'px';
+	};
+
+	useEffect(() => {
+		window.addEventListener('resize', resetHeight);
+		resetHeight();
+	}, []);
+
 	return (
 		<Provider store={store}>
 			<Layout>
